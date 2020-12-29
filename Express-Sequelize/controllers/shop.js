afcaeaba9700f18dcbde9f6e8101000635586ddc
Nur A Alam <nuraalam376@gmail.com>
2020-12-30 00:38:59 +0600

    practiced sequelize crud

--- Express-Sequelize/controllers/shop.js
+++ Express-Sequelize/controllers/shop.js
@@ -13,10 +13,10 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, (product) => {
+  Product.findAll({ where: { id: prodId } }).then((products) => {
     res.render("shop/product-detail", {
-      product: product,
-      pageTitle: product.title,
+      product: products[0],
+      pageTitle: products[0].title,
       path: "/products",
     });
   });
