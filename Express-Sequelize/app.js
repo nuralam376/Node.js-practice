03b97f666c3397f395107642d0ae6812c53c47d8
Nur A Alam <nuraalam376@gmail.com>
2021-01-01 01:28:37 +0600

    practiced sequlize relationships

--- Express-Sequelize/app.js
+++ Express-Sequelize/app.js
@@ -11,6 +11,8 @@ const sequelize = require("./util/database");
 
 const Product = require("./models/product");
 const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -39,6 +41,10 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
   // .sync({ force: true })
@@ -53,6 +59,9 @@ sequelize
     return user;
   })
   .then((user) => {
+    return user.createCart();
+  })
+  .then(() => {
     app.listen(3000, () => console.log("Server started on port 3000"));
   })
   .catch((err) => {
