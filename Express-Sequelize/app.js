c03a005993063e1e14e4e064a36313f96b3abc4e
Nur A Alam <nuraalam376@gmail.com>
2021-01-07 08:53:54 +0600

    practiced sequelize association

--- Express-Sequelize/app.js
+++ Express-Sequelize/app.js
@@ -2,7 +2,9 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const sequelize = require("./models/product");
+const sequelize = require("./util/database");
+const User = require("./models/user");
+const Product = require("./models/product");
 
 const errorController = require("./controllers/error");
 
@@ -22,8 +24,13 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+User.hasMany(Product);
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+
 sequelize
-  .sync()
+  .sync({
+    force: true,
+  })
   .then(() => {
     app.listen(3000, () => console.log("Server started on port 3000"));
   })
