eff9d997199210eeec5be3e44062ec7866ca8138
Nur A Alam <nuraalam376@gmail.com>
2021-08-03 18:13:34 +0600

    updated value

--- nest.js/carvalue/src/users/users.service.ts
+++ nest.js/carvalue/src/users/users.service.ts
@@ -11,4 +11,23 @@ export class UsersService {
     const user = this.repo.create({ email, password });
     return this.repo.save(user);
   }
+
+  findOne(id: number) {
+    return this.repo.findOne(id);
+  }
+
+  find(email: string) {
+    return this.repo.find({ email });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.repo.findOne(id);
+
+    if (!user) {
+      throw new Error('user not found');
+    }
+
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
 }
