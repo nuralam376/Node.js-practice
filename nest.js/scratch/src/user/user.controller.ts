47411e06946276294aa5a46039f9c99ec54bc35c
Nur A Alam <nuraalam376@gmail.com>
2022-01-25 23:13:48 +0600

    added controller and service methods

--- nest.js/scratch/src/user/user.controller.ts
+++ nest.js/scratch/src/user/user.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
+import { User } from "./interface/user.interface";
 import { UserService } from "./user.service";
 
 @Controller("user")
@@ -8,5 +9,15 @@ export class UserController {
     @Get()
     getUsers() {
         return this.userService.getAllUsers();
+    }
+
+    @Post() 
+    postUser(@Body() user: User) {
+        return this.userService.postUser(user);
+    }
+
+    @Delete("/:email")
+    deleteUser(@Param('email') email : string) {
+        return this.userService.deleteUser(email);
     }
 }
--- nest.js/scratch/src/user/user.service.ts
+++ nest.js/scratch/src/user/user.service.ts
@@ -1,8 +1,22 @@
-import { Injectable } from "@nestjs/common";
+import { Body, Injectable } from "@nestjs/common";
+import { User } from "./interface/user.interface";
 
 @Injectable()
 export class UserService {
-    getAllUsers() {
-        return "All Users";
+    public users : User[] = [];
+    getAllUsers() : User[] {
+        return this.users;
+    }
+
+    postUser(user : User) : User {
+        if(user) {
+            this.users.push(user);
+        }
+        return user;
+    }
+
+    deleteUser(email : string) : User [] {
+        const remainingUser = this.users.filter(user => user.email != email);
+        return remainingUser;
     }
 }
